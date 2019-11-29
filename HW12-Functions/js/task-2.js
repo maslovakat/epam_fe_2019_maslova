84c103121598ad0159f6ce7927ef9eb5e1874033
maslovakat <maslova.ekaterina94@gmail.com>
2019-11-29 13:25:54 +0200

    HW12-Functions: task-2: change solution to right way

--- HW12-Functions/js/task-2.js
+++ HW12-Functions/js/task-2.js
@@ -1,18 +1,34 @@
-function getSumOfStrings(firstString, ...rest) {
-  const result = [];
-  const resultLength = [firstString, ...rest];
 
-  resultLength.sort((a, b) => {
+function getSumOfStrings(firstString, secondString) {
+  const result = [];
+  let inMemory = 0;
+  let tmp = 0;
+  const sortedStrings = [firstString, secondString];
+
+  sortedStrings.sort((a, b) => {
     return b.length - a.length;
   });
 
-  for (let i = 0; i < resultLength[0].length; i++) {
-    result.push(String((+resultLength[0][i] || 0) + (+resultLength[1][i] || 0))); // we can add "+ (+resultLength[2][i] || 0)" to get sum of three srtings and so on
+  for (let i = sortedStrings[0].length - 1; i >= 0; i--) {
+    tmp = ((Number(sortedStrings[0][i])) + Number((sortedStrings[1][i - (sortedStrings[0].length - sortedStrings[1].length)] || 0)));
+    if (i === 0) {
+      result.unshift(Number(inMemory + tmp));
+    }
+    else if (tmp > 9) {
+      result.unshift(Number(tmp - 10 + inMemory));
+      inMemory = 1;
+    } else {
+      result.unshift(Number(tmp + inMemory));
+      inMemory = 0;
+    }
   }
   return result.join('');
 }
 
 getSumOfStrings('123', '324');
-getSumOfStrings('111111', '233333');
-getSumOfStrings('112233', '11223344');
+getSumOfStrings('11111111111111111111111111111111111111111111111111', '23333333333333333333333333333333333333333333333333');
+
+getSumOfStrings('234', '197');
+getSumOfStrings('999', '112');
+getSumOfStrings('1111', '11223344');
 getSumOfStrings('112233', '11');
